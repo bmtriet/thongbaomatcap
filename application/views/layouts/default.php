fb8920da86c18ad55254401b1c1fdadc0e88ce12
Anh Saker <anhsk.ohbo@gmail.com>
2013-11-15 21:43:14 +0700

    Loại bỏ inline-css

--- application/views/layouts/default.php
+++ application/views/layouts/default.php
@@ -17,13 +17,6 @@
 	<link rel="stylesheet" href="<?php echo base_url('assets/css/bootstrap.min.css') ?>">
 	<link rel="stylesheet" href="<?php echo base_url('assets/css/font-awesome.min.css') ?>">
 	<link rel="stylesheet" href="<?php echo base_url('assets/css/main.css') ?>">
-	
-	<style>
-		body {
-			padding-top: 50px;
-			padding-bottom: 20px;
-		}
-	</style>
 
 	<!-- Modernizr + Respond -->
 	<script src="<?php echo base_url('assets/js/vendor/modernizr-2.6.2-respond-1.1.0.min.js') ?>"></script>
